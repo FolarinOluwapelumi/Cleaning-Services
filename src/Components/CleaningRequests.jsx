e1c29c77eb1bcfcdbc02c0ab76b7ef560915ff33
Folarin <folarinoluwapelumi81@gmail.com>
2024-10-02 09:05:19 +0100

    first commit

---
+++ src/Components/CleaningRequests.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CleaningRequests = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold">Cleaning Requests</h2>
+      <p>Here are the cleaning requests available.</p>
+    </div>
+  );
+};
+
+export default CleaningRequests;
